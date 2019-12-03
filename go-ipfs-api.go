1dfc1f114332dcbc48610575439f88283939ac43
Kuro1 <412681778@qq.com>
2019-12-03 16:15:24 +0800

    Update go-ipfs-api.go

--- go-ipfs-api.go
+++ go-ipfs-api.go
@@ -3,22 +3,28 @@ package main
 import (
 	"bytes"
 	"fmt"
-	
+	"strings"
+
 	shell "github.com/ipfs/go-ipfs-api"
 )
 
 func main() {
 	sh := shell.NewLocalShell()
+	cid, err := sh.Add(strings.NewReader("hello world! jihao gogo"))
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(cid)
 	// get block
-	blo,err:=sh.BlockGet("Qmaisz6NMhDB51cCvNWa1GMS7LU1pAxdF4Ld6Ft9kZEP2a")
-	if err !=nil{
+	blo, err := sh.BlockGet("QmXnSYWiLndBSK69gboyVAsgpvJf1aE1SEfw7Mq65heiNB")
+	if err != nil {
 		panic(err)
 	}
 	fmt.Println(string(blo))
 
 	// cat obj
-	obj,err:=sh.Cat("Qmaisz6NMhDB51cCvNWa1GMS7LU1pAxdF4Ld6Ft9kZEP2a")
-	if err !=nil {
+	obj, err := sh.Cat("Qmaisz6NMhDB51cCvNWa1GMS7LU1pAxdF4Ld6Ft9kZEP2a")
+	if err != nil {
 		panic(err)
 	}
 	var buf bytes.Buffer
